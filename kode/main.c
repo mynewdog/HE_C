2226e4bfaa6237c205f2255ac77cb3d9bbb18ec4
nikkaUbuntu <erivalde@hotmail.com>
2018-11-04 14:44:25 -0500

    made seperate file function

--- kode/main.c
+++ kode/main.c
@@ -5,9 +5,10 @@
 
 char saltchars[]="abcdefghikjlmnopqrstuvWxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+@£[]}";
 
+FILE *openfile(char* filename, char* mode);
+
 int main(int argc, char* argv[])
 {
-    //Sjekk om argumenter er korrekt
     if(argc != 2)
     {
         printf("To use this program you need to input a hash value as second parameter");
@@ -15,52 +16,55 @@ int main(int argc, char* argv[])
     }
 
     char password[1000];
-
-    //input_hash er hash verdien som blir gitt ved input
     char *input_hash = argv[1];
-    //Så mye som skal lagres i salt
     char salt[16];
     
     //Kopierer over salt fra input_hash
     memcpy(salt, input_hash, 12);
   
-    FILE *ftpr = fopen("dictionary.txt", "r");
-    if(!ftpr)
-    {
-        printf("Error, cannot open file..\n");
-        return -1;
-    }
+    FILE* dictionary = openfile("crypto/dictionary.txt", "r");
 
     //Starten av dictionary attack
+
     printf("Searching for password in dictionary\n");
-    while(fgets(password, 1000, ftpr) != NULL) 
+    while(fgets(password, 1000, dictionary) != NULL) 
     {
         int length = strlen(password);
-        //Gjør om alle \n verdier til en null terminator
         if(password[length-1] == '\n')
         {
             password[length-1] = '\0';
         }
         
-        char* encrypted=crypt(password,salt);
-        char* compareEncrypted = encrypted;
+        char* encrypted_password = crypt(password,salt);
+        char* compareEncrypted = encrypted_password;
 
         if(strcmp(compareEncrypted, input_hash) == 0)
         {
-            printf("Found hash: %s\n", encrypted);
+            printf("Found hash: %s\n", encrypted_password);
             printf("The password is: %s\n", password);
             exit(1);
         }
         
     }
-
-    fclose(ftpr);
+    fclose(dictionary);
 
     printf("Password was not found in dictionary.\n");
     printf("Want to decide password length?");
 
-
-    
-
     return 0;
 }
+
+FILE *openfile(char* filename, char* mode)
+{
+    FILE *file;
+
+    file = fopen(filename, mode);
+    if(!file)
+    {
+        printf("Error, cannot open file..\n");
+        exit(1);
+    }
+
+    return file;
+}
+
