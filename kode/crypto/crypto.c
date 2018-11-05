f49b7e4e18e9f0d346a4e81521153ace05dafe18
nikkaUbuntu <erivalde@hotmail.com>
2018-11-05 18:03:25 -0500

    standard fil

--- kode/crypto/crypto.c
+++ kode/crypto/crypto.c
@@ -10,10 +10,10 @@ int main(int argc, char** argv)
 	char* passwd=argv[1];
 
 	char salt[13]="$1$abcdefgh$";
-	/*getrandom(salt+3,8,0);
+	getrandom(salt+3,8,0);
 	for(unsigned int i=3;i<11;i++){
 		salt[i]=saltchars[salt[i] & 0x3f];
-	}*/
+	}
 	char* encrypted=crypt(passwd,salt);
 	printf("%s\n", encrypted);
 	//free(encrypted);
